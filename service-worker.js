855a790fd1ada43e4701d8697a28a55d7ab31927
k015326 <92420550+k015326@users.noreply.github.com>
2026-04-23 15:52:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,23 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("cache").then(cache => {
+const CACHE_NAME = "chappin-v2";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
         "./app.js",
-        "./style.css"
+        "./style.css",
+        "./manifest.json"
       ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
